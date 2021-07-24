0a0c2861676b29304d4563a20494e6cb33049a40
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-24 18:33:09 -0300

    Formatted files

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = "terraform-bucket-${random_id.this.hex}"
-  acl = "private"
+  acl    = "private"
 
   tags = {
     "Name" = "terraform-bucket"
@@ -9,16 +9,16 @@ resource "aws_s3_bucket" "this" {
 
 resource "aws_s3_bucket_object" "json" {
   bucket = aws_s3_bucket.this.bucket
-  key = "/files/documents/countries.json"
-  acl = "public-read"
-  
+  key    = "/files/documents/countries.json"
+  acl    = "public-read"
+
   source = "files/countries.json"
 }
 
 resource "aws_s3_bucket_object" "jpg" {
   bucket = aws_s3_bucket.this.bucket
-  key = "/files/images/demon-slayer.jpg"
-  acl = "private"
-  
+  key    = "/files/images/demon-slayer.jpg"
+  acl    = "private"
+
   source = "files/demon-slayer.jpg"
 }
